4719321ee7679fedf2c7b59ca8bcaa05b9b7f78e
bah abdourahmane <bahabdourahimane@gmail.com>
2022-08-26 16:41:01 +0100

    Fin de la navigation

---
+++ script.js
@@ -0,0 +1,8 @@
+// navigation
+let menuIcon = document.querySelector(".menuIcon");
+let nav = document.querySelector(".nav");
+let hero = document.querySelector("#hero");
+menuIcon.addEventListener("click", ()=>{
+    hero.classList.toggle("show");
+})
+// --------------------------------------
